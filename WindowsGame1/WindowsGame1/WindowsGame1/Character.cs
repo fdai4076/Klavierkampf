11a577d7bb7bb134aa850174b0ca35bae8eadf6d
Michael Zinßer <Michael.Zinsser@gmx.de>
2015-07-08 11:31:28 +0200

    Character class

---
+++ WindowsGame1/WindowsGame1/WindowsGame1/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+
+public class Character
+{
+
+    private Model mesh;
+    //private Audio sound;
+    //private Boundingbox; 
+    private int geschwindigkeit;
+    private float gravity;
+
+
+    public Character(Model mesh, int geschwindigkeit, float gravity)
+	{
+        this.mesh = mesh;
+        this.geschwindigkeit = geschwindigkeit;
+        this.gravity = gravity;
+	}
+
+    public Model getMesh()
+    {
+        return mesh;
+    }
+
+    public int getGeschwindigkeit()
+    {
+        return geschwindigkeit;
+    }
+
+    public float getGravity()
+    {
+        return gravity;
+    }
+
+}
