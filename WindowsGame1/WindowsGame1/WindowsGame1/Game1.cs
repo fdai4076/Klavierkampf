70214387ac9aed80a036d5b4dc5763e056aa1a50
fdai4076 <sebastian.krah2@informatik.hs-fulda.de>
2015-09-12 16:53:50 +0200

    Gamestate: Logo = Fertig;            Splahscreen = Neue angeorndnet            Charakterwahl = Angelegt            Pause = Erneuert
    Versucht Array mit Texture2D´s Anzulegen ist aber Missglückt

--- WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
@@ -39,18 +39,25 @@ namespace WindowsGame1
         public CollisionManager collisionManager;
         
 
-        enum GameState { logo, splashMenu, character, howtoplay, option, ingame, pause, result };
+        enum GameState { logo, splashMenu, character, howtoplay, ingame, pause, result };
         private GameState gamestate;
 
+        int buttonMiddle;
         Button buttonPauseReturn, buttonPauseMainmenu;
-        Button buttonSplahscreenStart, buttonSplashscreenHowtoplay, buttonSplashscreenOption, buttonSplashscreenExit;
-        Button buttonOptionBack;
-        Button buttonHowtoplayBack;
+        Button buttonSplahscreenStart, buttonSplashscreenHowtoplay, buttonSplashscreenExit;
+        Button buttonCharakterBack, buttonCharakterFor;
+        Button player1Back, player1For, player2Back, player2For, player3Back, player3For, player4Back, player4For;
+        Button optionMute;
 
-        Texture2D pausenMenu;
-        Texture2D splashscreen, splashsreenButtonBackground;
-        Texture2D optionMenu;
-        Texture2D howtoplayscreen;
+        Texture2D buttonBackground, alroundscreen;
+        Texture2D pause;
+        Texture2D splashscreen;
+        Texture2D [] howtoplayscreen;
+        Texture2D logoPicture, logoBesch;
+        Texture2D charakterwahl, player1Char, player2Char, player3Char, player4Char;
+
+        Color logoBeschColor;
+        bool logoBeschInvisible;
 
         int screenWidth = 1280, screenHeight = 720;
 
@@ -76,11 +83,32 @@ namespace WindowsGame1
 
             base.Initialize();
             
-            gamestate = GameState.splashMenu;
+            gamestate = GameState.logo;
 
+            howtoplayscreen = new Texture2D[2];
 
+            logoBeschColor = new Color(255, 255, 255, 255);
 
-           
+            buttonMiddle = 565;
+            buttonPauseReturn.setPosition(new Vector2(buttonMiddle, 300));
+            buttonPauseMainmenu.setPosition(new Vector2(buttonMiddle, 390));
+            buttonSplahscreenStart.setPosition(new Vector2(buttonMiddle, 290));
+            buttonSplashscreenHowtoplay.setPosition(new Vector2(buttonMiddle, 380));
+            buttonSplashscreenExit.setPosition(new Vector2(buttonMiddle, 470));
+            buttonCharakterBack.setPosition(new Vector2(30, 640));
+            buttonCharakterFor.setPosition(new Vector2(1100, 640));
+
+            player1Back.setPosition(new Vector2(270, 250));
+            player1For.setPosition(new Vector2(455, 250));
+
+            player2Back.setPosition(new Vector2(770, 250));
+            player2For.setPosition(new Vector2(955, 250));
+
+            player3Back.setPosition(new Vector2(270, 450));
+            player3For.setPosition(new Vector2(455, 450));
+
+            player4Back.setPosition(new Vector2(770, 450));
+            player4For.setPosition(new Vector2(955, 450));
 
             arenaBounding = new BoundingBox(new Vector3(-12.5f, 1f, -12.5f), new Vector3(12.5f, 1f, 12.5f));
             groundBounding = new BoundingBox (new Vector3(-25f, -1f, -25f), new Vector3 (25f, -1f, 25f));
@@ -117,32 +145,34 @@ namespace WindowsGame1
             ultimatesphere = Content.Load<Model>("ultimateSphere");
             // TODO: use this.Content to load your game content here
 
-            pausenMenu = Content.Load<Texture2D>("Menu/Pause/PausenMenu");
+            alroundscreen = Content.Load<Texture2D>("Menu/Logo/Background");
+            logoPicture = Content.Load<Texture2D>("Menu/Logo/Logo3");
+            logoBesch = Content.Load<Texture2D>("Menu/Logo/LogoBesch");
             splashscreen = Content.Load<Texture2D>("Menu/splashMenu/SplashMenu");
-            splashsreenButtonBackground = Content.Load<Texture2D>("Menu/splashMenu/ButtonBackground2");
-            optionMenu = Content.Load<Texture2D>("Menu/Option/Background");
-            howtoplayscreen = Content.Load<Texture2D>("Menu/Howtoplay/Background");
+            buttonBackground = Content.Load<Texture2D>("Menu/splashMenu/ButtonBackground2");
+            charakterwahl = Content.Load<Texture2D>("Menu/Charakterwahl/Charakterwahl");
+            player1Char = Content.Load<Texture2D>("Menu/Charakterwahl/Char0");
+            player2Char = Content.Load<Texture2D>("Menu/Charakterwahl/Char0");
+            player3Char = Content.Load<Texture2D>("Menu/Charakterwahl/Char0");
+            player4Char = Content.Load<Texture2D>("Menu/Charakterwahl/Char0");
+            //howtoplayscreen[0] = Content.Load<Texture2D>("Menu/Howtoplay/Background");
+            pause = Content.Load<Texture2D>("Menu/Pause/Pause");
 
-            buttonPauseReturn = new Button(Content.Load<Texture2D>("Menu/Pause/returnButton"), Content.Load<Texture2D>("Menu/Pause/returnButton2"), graphics.GraphicsDevice);
-            buttonPauseReturn.setPosition(new Vector2(515, 300));
-
-            buttonPauseMainmenu = new Button(Content.Load<Texture2D>("Menu/Pause/MainMenu"), Content.Load<Texture2D>("Menu/Pause/MainMenu2"), graphics.GraphicsDevice);
-            buttonPauseMainmenu.setPosition(new Vector2(515, 475));
-
+            buttonPauseReturn = new Button(Content.Load<Texture2D>("Menu/Pause/ButtonReturn"), Content.Load<Texture2D>("Menu/Pause/ButtonReturn2"), graphics.GraphicsDevice);
+            buttonPauseMainmenu = new Button(Content.Load<Texture2D>("Menu/Pause/ButtonMainmenu"), Content.Load<Texture2D>("Menu/Pause/ButtonMainmenu2"), graphics.GraphicsDevice);
             buttonSplahscreenStart = new Button(Content.Load<Texture2D>("Menu/SplashMenu/StartButton"), Content.Load<Texture2D>("Menu/SplashMenu/StartButton2"), graphics.GraphicsDevice);
-            buttonSplahscreenStart.setPosition(new Vector2(565, 250));
-
             buttonSplashscreenHowtoplay = new Button(Content.Load<Texture2D>("Menu/SplashMenu/Howtoplay"), Content.Load<Texture2D>("Menu/SplashMenu/Howtoplay2"), graphics.GraphicsDevice);
-            buttonSplashscreenHowtoplay.setPosition(new Vector2(565, 340));
-
             buttonSplashscreenExit = new Button(Content.Load<Texture2D>("Menu/SplashMenu/ExitButton"), Content.Load<Texture2D>("Menu/SplashMenu/ExitButton2"), graphics.GraphicsDevice);
-            buttonSplashscreenExit.setPosition(new Vector2(565, 520));
-
-            buttonSplashscreenOption = new Button(Content.Load<Texture2D>("Menu/SplashMenu/OptionButton"), Content.Load<Texture2D>("Menu/SplashMenu/OptionButton2"), graphics.GraphicsDevice);
-            buttonSplashscreenOption.setPosition(new Vector2(565, 430 ));
-
-            buttonOptionBack = new Button(Content.Load<Texture2D>("Menu/Back"), Content.Load<Texture2D>("Menu/Back2"), graphics.GraphicsDevice);
-            buttonOptionBack.setPosition(new Vector2(25, 600));
+            buttonCharakterBack = new Button(Content.Load<Texture2D>("Menu/Back"), Content.Load<Texture2D>("Menu/Back2"), graphics.GraphicsDevice);
+            buttonCharakterFor = new Button(Content.Load<Texture2D>("Menu/For"), Content.Load<Texture2D>("Menu/For2"), graphics.GraphicsDevice);
+            player1Back = new Button(Content.Load<Texture2D>("Menu/Charakterwahl/CBack"), Content.Load<Texture2D>("Menu/Charakterwahl/CBack2"), graphics.GraphicsDevice);
+            player1For = new Button(Content.Load<Texture2D>("Menu/Charakterwahl/CFor"), Content.Load<Texture2D>("Menu/Charakterwahl/CFor2"), graphics.GraphicsDevice);
+            player2Back = new Button(Content.Load<Texture2D>("Menu/Charakterwahl/CBack"), Content.Load<Texture2D>("Menu/Charakterwahl/CBack2"), graphics.GraphicsDevice);
+            player2For = new Button(Content.Load<Texture2D>("Menu/Charakterwahl/CFor"), Content.Load<Texture2D>("Menu/Charakterwahl/CFor2"), graphics.GraphicsDevice);
+            player3Back = new Button(Content.Load<Texture2D>("Menu/Charakterwahl/CBack"), Content.Load<Texture2D>("Menu/Charakterwahl/CBack2"), graphics.GraphicsDevice);
+            player3For = new Button(Content.Load<Texture2D>("Menu/Charakterwahl/CFor"), Content.Load<Texture2D>("Menu/Charakterwahl/CFor2"), graphics.GraphicsDevice);
+            player4Back = new Button(Content.Load<Texture2D>("Menu/Charakterwahl/CBack"), Content.Load<Texture2D>("Menu/Charakterwahl/CBack2"), graphics.GraphicsDevice);
+            player4For = new Button(Content.Load<Texture2D>("Menu/Charakterwahl/CFor"), Content.Load<Texture2D>("Menu/Charakterwahl/CFor2"), graphics.GraphicsDevice);
 
 
         }
@@ -179,6 +209,14 @@ namespace WindowsGame1
             {
                 case GameState.logo:
                     
+                    if (Keyboard.GetState().IsKeyDown(Keys.Space))
+                        gamestate = GameState.splashMenu;
+
+                    if (logoBeschColor.A == 255) logoBeschInvisible = false;
+                    if (logoBeschColor.A == 0) logoBeschInvisible = true;
+                    if (logoBeschInvisible) logoBeschColor.A += 3;
+                    else logoBeschColor.A -= 3;
+
                     break;
 
                 case GameState.splashMenu:
@@ -187,7 +225,7 @@ namespace WindowsGame1
 
                     if (buttonSplahscreenStart.isClicked == true)
                     {
-                        gamestate = GameState.ingame;
+                        gamestate = GameState.character;
                         IsMouseVisible = false;
                         buttonSplahscreenStart.isClicked = false;
                     }
@@ -206,38 +244,85 @@ namespace WindowsGame1
                         this.Exit();
                     }
                     buttonSplashscreenExit.Update(mouse);
-
-                    if (buttonSplashscreenOption.isClicked == true)
-                    {
-                        gamestate = GameState.option;
-                        IsMouseVisible = false;
-                        buttonSplashscreenHowtoplay.isClicked = false;
-                    }
-                    buttonSplashscreenOption.Update(mouse);
                     
                     break;
 
                 case GameState.character:
                     
+                    IsMouseVisible = true;
+
+                    //Play1Charakterwahl Update
+                    if (player1Back.isClicked == true)
+                    {
+                        player1Back.isClicked = false;
+                    }
+                    player1Back.Update(mouse);
+
+                     if (player1For.isClicked == true)
+                    {
+                        player1For.isClicked = false;
+                    }
+                    player1For.Update(mouse);
+
+                    //Play2Charakterwahl Update
+                    if (player2Back.isClicked == true)
+                    {
+                        player2Back.isClicked = false;
+                    }
+                    player2Back.Update(mouse);
+
+                     if (player2For.isClicked == true)
+                    {
+                        player2For.isClicked = false;
+                    }
+                    player2For.Update(mouse);
+
+                    //Play3Charakterwahl Update
+                    if (player3Back.isClicked == true)
+                    {
+                        player3Back.isClicked = false;
+                    }
+                    player3Back.Update(mouse);
+
+                    if (player3For.isClicked == true)
+                    {
+                        player3For.isClicked = false;
+                    }
+                    player3For.Update(mouse);
+
+                    //Play4Charakterwahl Update
+                    if (player4Back.isClicked == true)
+                    {
+                        player4Back.isClicked = false;
+                    }
+                    player4Back.Update(mouse);
+
+                    if (player4For.isClicked == true)
+                    {
+                        player4For.isClicked = false;
+                    }
+                    player4For.Update(mouse);
+
+                    if (buttonCharakterBack.isClicked == true)
+                    {
+                        gamestate = GameState.splashMenu;
+                        IsMouseVisible = false;
+                        buttonCharakterBack.isClicked = false;
+                    }
+                    buttonCharakterBack.Update(mouse);
+
+                    if (buttonCharakterFor.isClicked == true)
+                    {
+                        gamestate = GameState.ingame;
+                        IsMouseVisible = false;
+                        buttonCharakterBack.isClicked = false;
+                    }
+                    buttonCharakterFor.Update(mouse);
+
                     break;
 
                 case GameState.howtoplay:
               
-                    break;
-
-                case GameState.option:
-
-                    IsMouseVisible = true;
-
-                    if (buttonOptionBack.isClicked == true)
-                    {
-                        gamestate = GameState.splashMenu;
-                        IsMouseVisible = false;
-                        buttonSplahscreenStart.isClicked = false;
-                    }
-
-                    buttonOptionBack.Update(mouse);
-
                     break;
 
                 case GameState.ingame:
@@ -311,37 +396,55 @@ namespace WindowsGame1
             {
                 case GameState.logo:
                     
+                    spriteBatch.Draw(alroundscreen, new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
+                    spriteBatch.Draw(logoPicture, new Rectangle(346, 0, logoPicture.Width, logoPicture.Height), Color.White);
+                    spriteBatch.Draw(logoBesch, new Rectangle(0, 420, logoBesch.Width, logoBesch.Height), logoBeschColor);
+
                     break;
 
                 case GameState.splashMenu:
                    
                     spriteBatch.Draw(splashscreen, new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
-                    spriteBatch.Draw(splashsreenButtonBackground, new Rectangle (490,150,splashsreenButtonBackground.Width,splashsreenButtonBackground.Height),Color.White);
+                    spriteBatch.Draw(buttonBackground, new Rectangle (490,150,buttonBackground.Width,buttonBackground.Height),Color.White);
                     buttonSplahscreenStart.Draw(spriteBatch);
                     buttonSplashscreenHowtoplay.Draw(spriteBatch);
-                    buttonSplashscreenOption.Draw(spriteBatch);
                     buttonSplashscreenExit.Draw(spriteBatch);
 
                     break;
 
                 case GameState.character:
                     
+                    spriteBatch.Draw(alroundscreen, new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
+                    spriteBatch.Draw(charakterwahl, new Rectangle(0, 0, charakterwahl.Width, charakterwahl.Height), Color.White);
+                    
+                    player1Back.Draw(spriteBatch);
+                    spriteBatch.Draw(player1Char, new Rectangle(335, 225, player1Char.Width, player1Char.Height), Color.White);
+                    player1For.Draw(spriteBatch);
+
+                    player2Back.Draw(spriteBatch);
+                    spriteBatch.Draw(player2Char, new Rectangle(835, 225, player2Char.Width, player2Char.Height), Color.White);
+                    player2For.Draw(spriteBatch);
+
+                    player3Back.Draw(spriteBatch);
+                    spriteBatch.Draw(player3Char, new Rectangle(335, 425, player3Char.Width, player3Char.Height), Color.White);
+                    player3For.Draw(spriteBatch);
+
+                    player4Back.Draw(spriteBatch);
+                    spriteBatch.Draw(player4Char, new Rectangle(835, 425, player4Char.Width, player4Char.Height), Color.White);
+                    player4For.Draw(spriteBatch);
+
+                    buttonCharakterBack.Draw(spriteBatch);
+                    buttonCharakterFor.Draw(spriteBatch);
+
                     break;
 
                 case GameState.howtoplay:
 
-                    spriteBatch.Draw(howtoplayscreen, new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
+                    spriteBatch.Draw(howtoplayscreen[0], new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
 
                     break;
 
-                case GameState.option:
-
-                    spriteBatch.Draw(optionMenu, new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
-                    buttonOptionBack.Draw(spriteBatch);
-
-                    break;
-
-                case GameState.ingame:
+               case GameState.ingame:
 
                    Matrix world = Matrix.Identity;
                     foreach (ModelMesh mesh in arena.Meshes)
@@ -382,7 +485,9 @@ namespace WindowsGame1
 
             case GameState.pause:
                 
-                spriteBatch.Draw(pausenMenu, new Rectangle(0,0, screenWidth,screenWidth), Color.White);
+                spriteBatch.Draw(alroundscreen, new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
+                spriteBatch.Draw(pause, new Rectangle(0, 0, pause.Width, pause.Height), Color.White);
+                spriteBatch.Draw(buttonBackground, new Rectangle(490, 150, buttonBackground.Width, buttonBackground.Height), Color.White);
                 buttonPauseReturn.Draw(spriteBatch);
                 buttonPauseMainmenu.Draw(spriteBatch);
                 
